7fe04d2006f065d71bb2c9289ae1c3ba57525d2e
dgDeathGamer <94063299+dgDeathGamer@users.noreply.github.com>
2024-07-08 14:14:35 -0400

    Delete Aplicacion_insana_2 directory
    probando

--- Aplicacion_insana_2/puntos.py/credenciales.py
+++
@@ -1,9 +0,0 @@
-def get_credenciales():
-    credenciales={
-        'host' : 'localhost',
-        'user' : 'root',
-        'password' : '',
-        'database' : 'sistema2',
-       # 'port' : '3306'
-    }
-    return credenciales
