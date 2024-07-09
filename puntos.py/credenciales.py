bca2d1378b6675d0b77c7c95e09c986e7f2728a8
dgDeathGamer <94063299+dgDeathGamer@users.noreply.github.com>
2024-07-08 20:21:16 -0400

    actualización de las credenciales
    tal cual, ahora es remoto c:

--- puntos.py/credenciales.py
+++ puntos.py/credenciales.py
@@ -1,9 +1,9 @@
 def get_credenciales():
     credenciales={
-        'host' : 'localhost',
-        'user' : 'root',
-        'password' : '',
-        'database' : 'sistema2',
+        'host' : 'sql10.freesqldatabase.com',
+        'user' : 'sql10718780',
+        'password' : 'yrxnDNiUiR',
+        'database' : 'sql10718780',
        # 'port' : '3306'
     }
     return credenciales
